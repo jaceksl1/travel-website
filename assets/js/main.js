edf9a5270d5b82410cab08974a476ce0c49f107d
Jacek <j.k.slomian@gmail.com>
2023-08-24 12:20:57 +0200

    adding html and css and js, finish for today

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,13 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () => {
+    const header = document.querySelector('#header');
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header');
+}
 
+window.addEventListener('scroll', blurHeader);
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
